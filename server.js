11adf9ce0d20dcdfbc27d9f0ad3833ebba1e9048
DevTaehong <minth1123@icloud.com>
2022-10-08 11:51:12 -0300

    Complete /register and /signin routes

--- server.js
+++ server.js
@@ -17,73 +17,50 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors()); // FOR CORS ERROR, you need this middle ware
 
-const database = {
-    users: [
-        {
-            id: '123', 
-            name: 'John', 
-            password: 'cookies',
-            email: 'john@gmail.com', 
-            entries: 0, 
-            joined: new Date()
-        },
-        {
-            id: '124', 
-            name: 'Taehong', 
-            password: 'bananas',
-            email: 'Taehong@gmail.com', 
-            entries: 0, 
-            joined: new Date()
-        },
-    ],
-    login: [
-        {
-            id: '987',
-            hash: '',
-            email: 'john@example.com'
-        }
-    ]
-}
-
 app.post('/signin', (req, res) => {
-    //     // Load hash from your password DB.
-    // bcrypt.compare("apples", '$2a$10$N1SUUrEcWWVRCZjWlZX3mOF5KYVW6wN0OxHXdZ3glgFy2NDi9LO7e', function(err, res) {
-    //     // res == true
-    //     console.log('first guess',res);
-    // });
-    // bcrypt.compare("veggies", '$2a$10$N1SUUrEcWWVRCZjWlZX3mOF5KYVW6wN0OxHXdZ3glgFy2NDi9LO7e', function(err, res) {
-    //     // res = falses
-    //     console.log('second guess',res);
-    // });
-    if (req.body.email === database.users[0].email && 
-        req.body.password === database.users[0].password){
-            res.json(database.users[0]);
-        } else {
-            res.status(400).json('error logging in');
-        }
+    knex.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+            if (isValid) {
+                return knex.select('*').from('users')
+                    .where('email', '=', req.body.email)
+                    .then(user => {
+                        res.json(user[0])
+                    })
+                    .catch(err => res.status(400).json('unable to get user'))
+            } else {
+                res.status(400).json('wrong credentials')
+            }
+        })
+        .catch(err => res.status(400).json('wrong credentials'))
 })
-
-// .insert({
-    // If you are using Knex.js version 1.0.0 or higher this 
-    // now returns an array of objects. Therefore, the code goes from:
-    // loginEmail[0] --> this used to return the email
-    // TO
-    // loginEmail[0].email --> this now returns the email
-//        email: loginEmail[0].email, // <-- this is the only change!
-//        name: name,
-//        joined: new Date()
-//   })
 
 app.post('/register', (req, res) => {
     const { email, password, name } = req.body;
-knex('users')
-    .returning('*')
-    .insert({
-        email: email,
-        name: name,
-        joined: new Date()
-    }).then(user => {
-        res.json(user[0]);
+    const hash = bcrypt.hashSync(password);
+    knex.transaction(trx => {
+        trx
+        .insert({
+            hash: hash,
+            email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+                .returning('*')
+                .insert({
+                    email: loginEmail[0].email,
+                    name: name,
+                    joined: new Date()
+                })
+                .then(user => {
+                    res.json(user[0]);
+                })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
     })
     .catch(err => res.status(400).json('unable to register'));
 })
@@ -114,14 +91,3 @@ knex('users').where('id', '=', id)
 app.listen(3000, () => {
     console.log("App is running on port 3000")
 })
-
-// .then(entries => {
-    // If you are using knex.js version 1.0.0 or higher this now 
-    // returns an array of objects. Therefore, the code goes from:
-    // entries[0] --> this used to return the entries
-    // TO
-    // entries[0].entries --> this now returns the entries
-//     res.json(entries[0].entries);
-//   })
-
-
