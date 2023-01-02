4e8e1ab19bdb9c06c91c8fb91e7119cc87cdf4bf
DevTaehong <minth1123@icloud.com>
2023-01-02 18:43:36 -0400

    Complete how old do I look application

--- controllers/image.js
+++ controllers/image.js
@@ -1,30 +1,58 @@
+//* REST API
 const Clarifai = require('clarifai');
+// console.log(Clarifai)
 
 const app = new Clarifai.App({
     apiKey: 'b58172beddae4925b565b284608ad97d'
 });
 
+//* gRPC
+// const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
+
+// const stub = ClarifaiStub.grpc();
+
+// const metadata = new grpc.Metadata();
+// metadata.set("authorization", "Key b58172beddae4925b565b284608ad97d");
+
 const handleApiCall = (req, res) => {
+    //* gRPC  
+    // stub.PostModelOutputs(
+    //     {
+            // This is the model ID of a publicly available General model. You may use any other public or custom model ID.
+    //         model_id: "age-demographics-recognition",
+    //         inputs: [
+    //             { data: { image: { url: req.body.input } } }
+    //         ]
+    //     },
+    //     metadata,
+    //     (err, response) => {
+    //         if (err) {
+    //             throw new Error(err);
+    //         }
+
+    //         if (response.status.code !== 10000) {
+    //             throw new Error("Post model outputs failed, status: " + response.status.description);
+    //         }
+
+            // Since we have one input, one output will exist here.
+    //         const output = response.outputs[0];
+
+    //         console.log("Predicted concepts:");
+    //         for (const concept of output.data.concepts) {
+    //             console.log(concept.name + " " + concept.value);
+    //         }
+    //         res.json(response)
+    //     }
+    // );
+    //* REST
     app.models  // Security review 12:06
-        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input) // this.state.imageUrl cannot work here because of how this.setState works.
+        .predict("age-demographics-recognition", req.body.imageFileUrl) // this.state.imageUrl cannot work here because of how this.setState works.
         .then(data => {
             res.json(data);
         })
         .catch(err => res.status(400).json('unable to work with API'));
 }
 
-const handleImage = (req, res, knex) => { // To update, use put
-    const { id } = req.body; 
-knex('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-        res.json(entries[0].entries);  
-    })
-    .catch(err => res.status(400).json('unable to get entries'))
-}
-
 module.exports = {
-    handleImage,
     handleApiCall
 }
--- server.js
+++ server.js
@@ -25,7 +25,6 @@ app.get('/', (req, res) => { res.send("It's working"); })
 app.post('/signin', signin.handleSignin(knex, bcrypt)) 
 app.post('/register', (req, res) => { register.handleRegister(req, res, knex, bcrypt) }) // This is called dependency injection
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, knex) })
-app.put('/image', (req, res) => { image.handleImage(req, res, knex) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
 app.listen(process.env.PORT || 3000, () => {
